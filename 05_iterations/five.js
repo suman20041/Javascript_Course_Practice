e56c91f0c391e2921613cadf778f1b2f429707d3
suman panda <137888386+suman20041@users.noreply.github.com>
2025-08-12 17:29:05 +0000

    foreach loop

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,1 +1,16 @@
 const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach(function (val) {
+//     console.log(val);
+// })
+
+// coding.forEach((item) => {
+//     console.log(item);
+// })
+
+
+function printMe(item) {
+    console.log(item);
+}
+
+coding.forEach(printMe)  //Here we only give the function reference printing work done by themself. we not execute the function here.
