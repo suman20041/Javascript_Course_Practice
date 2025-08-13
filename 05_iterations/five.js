5461f7eab2655674bbf6f253537dd18bf3cda14a
suman panda <137888386+suman20041@users.noreply.github.com>
2025-08-13 05:59:39 +0000

    forEach loop

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -9,8 +9,32 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 // })
 
 
-function printMe(item) {
-    console.log(item);
-}
+// function printMe(item) {
+//     console.log(item);
+// }
+// coding.forEach(printMe)  //Here we only give the function reference printing work done by themself. we not execute the function here.
 
-coding.forEach(printMe)  //Here we only give the function reference printing work done by themself. we not execute the function here.
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+
+const myCoding = [
+    {
+        languageName:"javaScript",
+        languageFileName:"js"
+    },
+    {
+        languageName:"java",
+        languageFileName:"java"
+    },
+    {
+        languageName:"python",
+        languageFileName:"py"
+    },
+]
+
+myCoding.forEach((item)=> {
+    console.log(item.languageName);
+})
