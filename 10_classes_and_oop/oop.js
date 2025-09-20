42cf06df0ba15e8222c6725dbf52e2b67183549e
suman panda <137888386+suman20041@users.noreply.github.com>
2025-09-20 18:12:49 +0000

    oop in js

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -8,11 +8,23 @@ const user = {
 
     //method
     getUserDetails: function(){
-        console.log("Got user deatils from database");
-        
+        //console.log("Got user deatils from database");
+        // console.log(`Username: ${this.username}`);
+
+        //this keyword present in function context
+        console.log(this);  // output: total object or current context
     }
 
 }
 
-console.log(user.username);
-console.log(user.getUserDetails()); // it is method getUserDetails so we need to execute() not onlly give reference
+//console.log(user.username);
+// console.log(user.getUserDetails()); // it is method getUserDetails so we need to execute() not onlly give reference
+
+//this keyword present in global context
+// console.log(this);  // output is empty object{}
+
+
+
+//Constructor function
+const promiseOne = new Promise()
+const date = new Date() //new keyword is constructor function and constructor function allow you to create multiple instances from a single object literal
