7878029a9c747f43063fb00a4a28479c85837801
suman panda <137888386+suman20041@users.noreply.github.com>
2025-09-22 18:08:01 +0000

    Prototype in js

--- 10_classes_and_oop/Prototype.js
+++ 10_classes_and_oop/Prototype.js
@@ -1,1 +1,20 @@
-let myName = "suman"
+// let myName = "suman     "
+// let myChannel = "chai     "
+
+// // console.log(myName.trim().length)
+// console.log(myName.trueLength());    //Those string have extra space that all string bydefault have this method .trueLength... but this line is not working here.
+
+
+
+let myHeros = ["thor", "spiderman"]
+
+let heroPower = {
+    thor: "hammer",
+    spiderman:"sling",
+
+
+    //method define
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`)
+    }
+}
