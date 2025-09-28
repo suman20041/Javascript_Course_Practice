ba231db0d5928b6a2ebfb950a5ae0d1e29ff1a45
suman panda <137888386+suman20041@users.noreply.github.com>
2025-09-28 08:59:53 +0000

    mathpi in js

--- 10_classes_and_oop/mathpi.js
+++ 10_classes_and_oop/mathpi.js
@@ -36,3 +36,46 @@ for (let [key, value] of Object.entries(chai)) {
         console.log(`${key}: ${value}`);
     }
 }
+
+// use defineProperties method
+const chai2 = {
+    name: "ginger chai",
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function () {
+        console.log("chai nhi bni");
+    }
+};
+
+// Before changing descriptors
+console.log("Before:", Object.getOwnPropertyDescriptor(chai2, "name"));
+
+// Using defineProperties to modify multiple properties
+Object.defineProperties(chai2, {
+    name: {
+        writable: false,     // cannot change name
+        enumerable: true,    // can show in iteration
+        configurable: true   // can redefine later
+    },
+    price: {
+        writable: true,      // can change price
+        enumerable: true,    // will show in iteration
+        configurable: true
+    },
+    isAvailable: {
+        writable: true,      
+        enumerable: false,   // will NOT show in iteration
+        configurable: true
+    }
+});
+
+// After changing descriptors
+console.log("After:", Object.getOwnPropertyDescriptor(chai2, "name"));
+console.log("After:", Object.getOwnPropertyDescriptor(chai2, "price"));
+console.log("After:", Object.getOwnPropertyDescriptor(chai2, "isAvailable"));
+
+// Iterating to check enumerable properties
+for (let key in chai2) {
+    console.log(key, ":", chai2[key]);
+}
