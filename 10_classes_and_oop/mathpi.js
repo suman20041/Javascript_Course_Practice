283281862e418b82db65b96f3f8ec826d0aedad7
suman panda <137888386+suman20041@users.noreply.github.com>
2025-09-28 08:52:44 +0000

    mathpi in js

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,38 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI");  //math module we want pi value
+
+// console.log(descripter);   
+
+
+// console.log(Math.PI);
+// Math.PI = 5   
+// console.log(Math.PI);   //Math.PI value can't be changed
+
+
+// const mynewObject = Object.create(null)
+const chai = {
+    name: "ginger chai",
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function() {
+        console.log("chai nhi bni");
+    }
+}
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));  
+
+
+
+//we can also change our own property but not in all place only limited place
+
+Object.defineProperty(chai, 'name', {
+    // writable: false,
+    //enumerable: false, // when we write enumerable: false then stop to happen enumaration or iteration
+    enumerable: true,  //when we write enumerable: true then start to happen enumaration or iteration
+}) //we can change property by using definePropery method
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));  
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') { //agar hai tho enumearte karege agar nehi hai tho hamara code nehi phatege
+        console.log(`${key}: ${value}`);
+    }
+}
