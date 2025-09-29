993e3fe5b94fe54be44fa952206cd2abd8a4223d
suman panda <137888386+suman20041@users.noreply.github.com>
2025-09-29 18:27:58 +0000

    getter and setter in js

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -1,10 +1,31 @@
+// Look good syntactically- here we give own customize method like we give the decrepted password or encrypted password
 //class is one wrapper here under the hood work prototype
-class User {
-    constructor(email, password) {
+class User {  
+    constructor(email, password) {   //constructor only set the value of email  not password, because i override the password
         this.email = email;
         this.password = password;
     }
+
+    // .email and ._email both are different...  ._email is a private property only access inside class
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email = value
+    }
+
+    get password(){  //In all the properties getter and setter automatically made as a method
+        return `${this._password}suman` //give different property name and override the password
+    }
+
+    set password(value){
+        this._password = value //give different property name and override the password
+    }
+    //database when i save value then exact value set but in get if someone want to access this value then not given exact value
 }
 
-const suman = new User("s@suman.ai", "123");
+const suman = new User("s@suman.ai", "abc");
 console.log(suman.password);
+console.log(suman._email);
